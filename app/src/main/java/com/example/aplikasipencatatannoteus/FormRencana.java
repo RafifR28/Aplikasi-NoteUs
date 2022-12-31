2e16cc3fba20f43a5a9089dfbac3d7ac23301ed5
Rafif Ramadan <ramadanrafif30@gmail.com>
2022-12-31 11:59:24 +0700

    Aplikasi NoteUs

---
+++ app/src/main/java/com/example/aplikasipencatatannoteus/FormRencana.java
@@ -0,0 +1,57 @@
+package com.example.aplikasipencatatannoteus;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class FormRencana extends AppCompatActivity {
+
+    EditText edtCatatan;
+    FirebaseFirestore fireDb;
+    FirebaseUser user;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_rencana);
+        edtCatatan = findViewById(R.id.edtRencana);
+
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        fireDb = FirebaseFirestore.getInstance();
+
+    }
+
+    public void tambahRencana(View view) {
+        String userId = user.getUid();
+        String email = user.getEmail();
+        String catatan = edtCatatan.getText().toString();
+
+        fireDb.collection("catatan").document()
+                .set(new Catatan(userId, email, catatan))
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w("store_error", e.getMessage());
+                    }
+                });
+        finish();
+    }
+
+    public void lihatRencana(View view) {
+        startActivity(new Intent(this, ListRencana.class));
+    }
+}
